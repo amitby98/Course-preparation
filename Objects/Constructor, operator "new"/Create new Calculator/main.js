f72dd9f7a3ddecb4e886acd5eb80454b63ac0667
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-16 23:03:29 +0200

    Calculator question
    Calculator question in the object new chapter

---
+++ Objects/Constructor, operator "new"/Create new Calculator/main.js
@@ -0,0 +1,11 @@
+function Calculator() {
+  
+    read
+    sum
+    nul
+
+let calculator = new Calculator();
+calculator.read();
+
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());
