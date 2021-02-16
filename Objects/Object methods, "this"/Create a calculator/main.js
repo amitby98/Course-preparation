990e5bfcdef8a70affff925dd0d9a1b069f3e0ed
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-16 17:23:34 +0200

    solved calculator question

--- Objects/Object methods, "this"/Create a calculator/main.js
+++ Objects/Object methods, "this"/Create a calculator/main.js
@@ -1,15 +1,19 @@
 let calculator = {
+  // read(a, b) {
+  //   this.num1 = a;
+  //   this.num2 = b;
+  // },
+  read() {
+    this.num1 = +prompt("num1?", 0);
+    this.num2 = +prompt("num2?", 0);
+  },
+
   sum() {
     return this.num1 + this.num2;
   },
 
   mul() {
     return this.num1 * this.num2;
-  },
-
-  read() {
-    this.num1 = +prompt("num1?", 0);
-    this.num2 = +prompt("num2?", 0);
   },
 };
 
